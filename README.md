8b465ac69bb1b5e31a23ad7cf6fdf76f4fa6660a
SB <seungbae2@gmail.com>
2021-05-25 17:05:22 +0900

    first init

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# BookReview
+# Book Review
+
+# 결과화면
+
+![1](./screenshot/1.png)
+
+
+
+![2](./screenshot/2.png)
+
+
+
+# 이 챕터를 통해 배우는 것
+
+- **RecyclerView** 사용하기
+- **View Binding** 사용하기
+- **Retrofit** 사용하기 (API 호출)
+- **Glide** 사용하기 (이미지 로딩)
+- **Android Room** 사용하기 (복습 파트2, 챕터4 계산기, Local DB)
+- Open API 사용해보기
+
+
+
+### 도서 리뷰 앱
+
+인터파크 Open API 를 통해 베스트셀러 정보를 가져와서 화면에 그릴 수 있음.
+
+인터파크 Open API 를 통해 검색어에 해당하는 책 목록을 가져와서 화면에 그릴 수 있음.
+
+Local DB 를 이용하여 검색 기록을 저장하고 삭제할 수 있음.
+
+Local DB 를 이용하여 개인 리뷰를 저장할 수 있음.
---
+++ app/src/main/java/com/example/bookreview/api/BookService.kt
@@ -0,0 +1,22 @@
+package com.example.bookreview.api
+
+import com.example.bookreview.model.BestSellerDto
+import com.example.bookreview.model.SearchBookDto
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface BookService {
+
+    @GET("/api/search.api?output=json")
+    fun getBooksByName(
+        @Query("key") apiKey: String,
+        @Query("query") keyword: String
+    ): Call<SearchBookDto>
+
+    @GET("/api/bestSeller.api?output=json&categoryId=100")
+    fun getBestSellerBooks(
+        @Query("key") apiKey: String
+    ): Call<BestSellerDto>
+
+}
